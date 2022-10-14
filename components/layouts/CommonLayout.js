0d7c12dc7b5d3ad43ac0ed6680f6299a8246c7c0
Abdul Basir <basir.bsmrstu@gmail.com>
2022-10-14 12:41:33 +0600

    meta data added

--- components/layouts/CommonLayout.js
+++ components/layouts/CommonLayout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import React from 'react'
 import Footer from '../Footer'
 import Navbar from '../Navbar'
@@ -5,6 +6,9 @@ import Navbar from '../Navbar'
 export default function CommonLayout({ children }) {
     return (
         <>
+            <Head>
+                <meta name="google-site-verification" content="jCeoP5AHSxiuJ-MSP2Hyst-GX2Tzfb2fQIaspqYUW0Y" />
+            </Head>
             <Navbar />
             <main className='lg:px-12 md:px-8 font-mono px-3'> {children} </main>
             <Footer />
